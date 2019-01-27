1cf0c0b5c612c78885fafcb1ec71a4c277b8ec0a
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2019-01-28 02:05:48 +0900

    Fix psgi

--- psgi/docker-entrypoint.sh
+++ psgi/docker-entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+set -e
+
+if [ ! -f mt-config.cgi ]; then
+    cp /root/mt-config.cgi.default mt-config.cgi
+fi
+
 # http://stackoverflow.com/questions/31746182/docker-compose-wait-for-container-x-before-starting-y
 while ! nc -z mysql 3306; do sleep 3; done
-start_server --port=3000 --pid-file=/app/run/movabletype.pid -- plackup -s Starlet --max-workers=2 mt.psgi
+
+exec "$@"
 
